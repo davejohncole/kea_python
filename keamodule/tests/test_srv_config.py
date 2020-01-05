053c41ab4558f3f1f3fef2fd782b24bcf813d949
djc <davejohncole@gmail.com>
2020-01-05 21:21:19 +1100

    Added more SrvConfig tests.

--- keamodule/tests/test_srv_config.py
+++ keamodule/tests/test_srv_config.py
@@ -2,13 +2,9 @@ import kea
 import utils
 
 
-class TestSrvConfig_new(utils.BaseTestCase):
+class TestGetCurrentConfig(utils.BaseTestCase):
 
     def test_ok(self):
-        s = kea.CfgMgr().getCurrentCfg()
-        self.assertEqual(3, s.use_count)
-
-    def test_get_current_cfg(self):
         m = kea.CfgMgr()
         c = m.getCurrentCfg()
         self.assertEqual(3, c.use_count)
@@ -16,7 +12,19 @@ class TestSrvConfig_new(utils.BaseTestCase):
         self.assertEqual(4, c.use_count)
         self.assertEqual(4, d.use_count)
 
-    def test_staging(self):
+    def test_bad_args(self):
+        m = kea.CfgMgr()
+        with self.assertRaises(TypeError) as cm:
+            m.getCurrentCfg(1)
+        self.assertEqual(('getCurrentCfg() takes no arguments (1 given)',), cm.exception.args)
+        with self.assertRaises(TypeError) as cm:
+            m.getCurrentCfg(x=1)
+        self.assertEqual(('getCurrentCfg() takes no keyword arguments',), cm.exception.args)
+
+
+class TestGetStagingConfig(utils.BaseTestCase):
+
+    def test_ok(self):
         m = kea.CfgMgr()
         c = m.getStagingCfg()
         self.assertEqual(2, c.use_count)
@@ -24,11 +32,28 @@ class TestSrvConfig_new(utils.BaseTestCase):
         self.assertEqual(3, c.use_count)
         self.assertEqual(3, d.use_count)
 
+    def test_bad_args(self):
+        m = kea.CfgMgr()
+        with self.assertRaises(TypeError) as cm:
+            m.getStagingCfg(1)
+        self.assertEqual(('getStagingCfg() takes no arguments (1 given)',), cm.exception.args)
+        with self.assertRaises(TypeError) as cm:
+            m.getStagingCfg(x=1)
+        self.assertEqual(('getStagingCfg() takes no keyword arguments',), cm.exception.args)
+
 
 class TestSrvConfig_toElement(utils.BaseTestCase):
 
     def test_ok(self):
-        m = kea.CfgMgr()
-        c = m.getStagingCfg()
+        c = kea.CfgMgr().getStagingCfg()
         e = c.toElement()
         self.assertIsInstance(e, dict)
+
+    def test_bad_args(self):
+        c = kea.CfgMgr().getStagingCfg()
+        with self.assertRaises(TypeError) as cm:
+            c.toElement(1)
+        self.assertEqual(('toElement() takes no arguments (1 given)',), cm.exception.args)
+        with self.assertRaises(TypeError) as cm:
+            c.toElement(x=1)
+        self.assertEqual(('toElement() takes no keyword arguments',), cm.exception.args)
