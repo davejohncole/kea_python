9afabad94cdd09b88a7144aba8726a99df6cb30c
djc <davejohncole@gmail.com>
2025-01-30 14:32:10 +1100

    Fix tests.

--- keamodule/tests/test_pkt4.py
+++ keamodule/tests/test_pkt4.py
@@ -430,10 +430,17 @@ class TestPkt4_toText(utils.BaseTestCase):
 
     def test_empty(self):
         p = kea.Pkt4(kea.DHCPREQUEST, 42)
-        self.assertEqual(textwrap.dedent("""\
-            local_address=0.0.0.0:67, remote_address=0.0.0.0:68, msg_type=DHCPREQUEST (3), transid=0x2a,
-            options:
-              type=053, len=001: 3 (uint8)"""), p.toText())  # noqa: E501
+        if kea.__version__ >= '2.6.1':
+            self.assertEqual(textwrap.dedent("""\
+                local_address=0.0.0.0:67, remote_address=0.0.0.0:68,
+                msg_type=DHCPREQUEST (3), trans_id=0x2a,
+                options:
+                  type=053, len=001: 3 (uint8)"""), p.toText())
+        else:
+            self.assertEqual(textwrap.dedent("""\
+                local_address=0.0.0.0:67, remote_address=0.0.0.0:68, msg_type=DHCPREQUEST (3), transid=0x2a,
+                options:
+                    type=053, len=001: 3 (uint8)"""), p.toText())  # noqa: E501
 
     def test_filled(self):
         p = kea.Pkt4(kea.DHCPREQUEST, 42)                                               # 53
@@ -446,13 +453,26 @@ class TestPkt4_toText(utils.BaseTestCase):
         p.addOption(kea.Option(kea.DHO_DHCP_LEASE_TIME).setUint32(7200))                # 51
         p.addOption(kea.Option(kea.DHO_DHCP_RENEWAL_TIME).setUint32(1800))              # 58
         p.addOption(kea.Option(kea.DHO_DHCP_REBINDING_TIME).setUint32(3600))            # 59
-        self.assertEqual(textwrap.dedent("""\
-            local_address=1.2.3.4:67, remote_address=2.3.4.5:68, msg_type=DHCPREQUEST (3), transid=0x2a,
-            options:
-              type=001, len=004: ff:ff:f0:00
-              type=003, len=004: 0a:00:00:01
-              type=015, len=008: 74:65:73:74:2e:6f:72:67
-              type=051, len=004: 00:00:1c:20
-              type=053, len=001: 3 (uint8)
-              type=058, len=004: 00:00:07:08
-              type=059, len=004: 00:00:0e:10"""), p.toText())  # noqa: E501
+        if kea.__version__ >= '2.6.1':
+            self.assertEqual(textwrap.dedent("""\
+                local_address=1.2.3.4:67, remote_address=2.3.4.5:68,
+                msg_type=DHCPREQUEST (3), trans_id=0x2a,
+                options:
+                  type=001, len=004: ff:ff:f0:00
+                  type=003, len=004: 0a:00:00:01
+                  type=015, len=008: 74:65:73:74:2e:6f:72:67
+                  type=051, len=004: 00:00:1c:20
+                  type=053, len=001: 3 (uint8)
+                  type=058, len=004: 00:00:07:08
+                  type=059, len=004: 00:00:0e:10"""), p.toText())
+        else:
+            self.assertEqual(textwrap.dedent("""\
+                local_address=1.2.3.4:67, remote_address=2.3.4.5:68, msg_type=DHCPREQUEST (3), transid=0x2a,
+                options:
+                type=001, len=004: ff:ff:f0:00
+                type=003, len=004: 0a:00:00:01
+                type=015, len=008: 74:65:73:74:2e:6f:72:67
+                type=051, len=004: 00:00:1c:20
+                type=053, len=001: 3 (uint8)
+                type=058, len=004: 00:00:07:08
+                    type=059, len=004: 00:00:0e:10"""), p.toText())  # noqa: E501
